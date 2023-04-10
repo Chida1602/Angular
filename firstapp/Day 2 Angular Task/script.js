f778708e023f246718041d9cf18d5422d7950ebc
Chida1602 <maasandramchidanand@gmail.com>
2023-04-10 14:00:15 +0530

    first commit

--- firstapp/Day 2 Angular Task/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelector("#login").addEventListener("click",function(){
-    document.querySelector(".pop-up").classList.add("active");
-});
-document.querySelector(".pop-up .close-button").addEventListener("click",function(){
-    document.querySelector(".pop-up").classList.remove("active");
-});
